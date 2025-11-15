79caff1c35c1fec78a3fa516977af01b80ce9e13
arpanghosh2416 <arpanghosh2416@gmail.com>
2025-11-16 03:00:59 +0530

    Original State

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // static export
-  images: {
-    unoptimized: true, // needed if you use next/image on static hosting
-  },
-  // optional but often useful on static hosting:
-  trailingSlash: true,
+  /* config options here */
+  reactCompiler: true,
 };
 
 export default nextConfig;
