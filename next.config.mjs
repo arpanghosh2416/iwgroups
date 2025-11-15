264e326dda5c8886155083bcab2b493fc98c80d1
arpanghosh2416 <arpanghosh2416@gmail.com>
2025-11-16 02:19:34 +0530

    Initializing CI/CD for Hostinger

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export", // static export
+  images: {
+    unoptimized: true, // needed if you use next/image on static hosting
+  },
+  // optional but often useful on static hosting:
+  trailingSlash: true,
 };
 
 export default nextConfig;
